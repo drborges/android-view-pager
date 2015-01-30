48a3a2a040171a13209334a19780a109aad2bac2
Diego Borges <drborges.cic@gmail.com>
2015-01-30 00:51:36 -0200

    simple example of a ViewPager

---
+++ app/src/main/java/drborges/me/viewpager/InjectableFragmentActiviity.java
@@ -0,0 +1,14 @@
+package drborges.me.viewpager;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import butterknife.ButterKnife;
+
+public class InjectableFragmentActiviity extends FragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/drborges/me/viewpager/TodoActivity.java
@@ -0,0 +1,48 @@
+package drborges.me.viewpager;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+
+public class TodoActivity extends InjectableFragmentActiviity {
+
+    public static final int NUM_PAGES = 2;
+
+    private PagerAdapter pagerAdapter;
+
+    @InjectView(R.id.pager) ViewPager pager;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo);
+        ButterKnife.inject(this);
+
+        pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        pager.setAdapter(pagerAdapter);
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return new TodoFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+    }
+}
